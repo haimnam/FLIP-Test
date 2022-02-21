f16d993afdc587f38c10c4089c35717b5a336713
haimnam <haimnam10@gmail.com>
2022-02-21 18:19:32 +0900

    Modify useContext

--- src/Store/FlipNoteContext.tsx
+++ src/Store/FlipNoteContext.tsx
@@ -1,13 +1,28 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useContext } from "react";
 
-export const NoteContext = createContext({noteLanguage: "English", clickLeft: () => {}, clickRight: () => {}});
+export const NoteContext = createContext({noteLang: "en", clickLeft: () => {}, clickRight: () => {}});
+
+export const useNoteLang = () => {
+    const { noteLang } = useContext(NoteContext);
+    return noteLang;
+}
+
+export const useClickLeft = () => {
+    const { clickLeft } = useContext(NoteContext);
+    return clickLeft;
+}
+
+export const useClickRight = () => {
+    const { clickRight } = useContext(NoteContext);
+    return clickRight;
+}
 
 const FlipNoteContext = ({ children }) => {
     const clickLeft = () => {
         setNoteLanguage(prevState => {
             return {
                 ...prevState,
-                noteLanguage: "Korean"
+                noteLang: "ko"
             }
         });
     }
@@ -15,11 +30,11 @@ const FlipNoteContext = ({ children }) => {
         setNoteLanguage(prevState => {
             return {
                 ...prevState,
-                noteLanguage: "English"
+                noteLang: "en"
             }
         });
     }
-    const [noteLanguage, setNoteLanguage] = useState({noteLanguage: "English", clickLeft, clickRight});
+    const [noteLanguage, setNoteLanguage] = useState({noteLang: "en", clickLeft, clickRight});
     return (
         <NoteContext.Provider value={noteLanguage}>{children}</NoteContext.Provider>
     );
