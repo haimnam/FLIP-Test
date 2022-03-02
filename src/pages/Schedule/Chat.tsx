bf484ea175bd7d85eca1fc65e0a7d53cab591e1d
haimnam <haimnam10@gmail.com>
2022-03-03 00:28:00 +0900

    Add a Chatting Function

--- src/pages/Schedule/Chat.tsx
+++ src/pages/Schedule/Chat.tsx
@@ -1,10 +1,20 @@
-import { time } from "console";
-import React from "react";
+import React, { useState } from "react";
 import styles from "../../scss/ScheduleAndChat.module.scss";
 import { PartnerInfoData } from "./PartnerInfoData.tsx";
 import { ChatPartnerData, ChatMyData } from "./ChatData.tsx";
+import SendIcon from "@mui/icons-material/Send";
 
 const Chat = () => {
+  const [chat, setChat] = useState({ input: "", chats: [] });
+
+  const onChangeChat = (e) => {
+    setChat({ input: e.target.value, chats: chat.chats });
+  };
+
+  const onClickChat = (e) => {
+    setChat({ input: "", chats: chat.chats.concat(chat.input) });
+  };
+
   return (
     <div className={styles.chatBody}>
       <div className={styles.chatInfo}>
@@ -72,9 +82,36 @@ const Chat = () => {
               </div>
             );
           })}
+
+          {/*<div className={styles.chatMyItem}>
+            <div className={styles.chatBox}>
+              <div className={styles.chatMessage}>{chat}</div>
+            </div>
+        </div>*/}
+          {chat.chats.map((ch, index) => {
+            return (
+              <div key={index} className={styles.chatMyItem}>
+                <div className={styles.chatBox}>
+                  <div className={styles.chatMessage}>{ch}</div>
+                </div>
+              </div>
+            );
+          })}
         </div>
         <div className={styles.chatInput}>
-          <input type="text" placeholder="write message..."></input>
+          <input
+            type="text"
+            placeholder="write message..."
+            value={chat.input}
+            onChange={onChangeChat}
+          ></input>
+          <button
+            type="submit"
+            onClick={onClickChat}
+            className={styles.sendIcon}
+          >
+            <SendIcon />
+          </button>
         </div>
       </div>
     </div>
