496316d80cac50db9c9d1d749c6a7a161b86b9e2
haimnam <haimnam10@gmail.com>
2022-03-04 03:41:43 +0900

    Enable Checkboxes in Preferred Times + Modify Timezone Converter

--- src/pages/Schedule/PreferredTimes.tsx
+++ src/pages/Schedule/PreferredTimes.tsx
@@ -1,8 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../../scss/ScheduleAndChat.module.scss";
-import { TimesData } from "./TimesData.tsx";
 
 const PreferredTimes = () => {
+  const [timesData, setTimesData] = useState([
+    {
+      id: 1,
+      main: "Tuesday 3:00 pm",
+      sub: "Wednesday 5:00 am KST",
+      isPartnerPick: false,
+      myPick: "sq",
+      state: "Finalize",
+    },
+    {
+      id: 2,
+      main: "Tuesday 3:00 pm",
+      sub: "Wednesday 5:00 am KST",
+      isPartnerPick: true,
+      myPick: "sq",
+      state: "Finalize",
+    },
+    {
+      id: 3,
+      main: "Tuesday 3:00 pm",
+      sub: "Wednesday 5:00 am KST",
+      isPartnerPick: true,
+      myPick: "sq",
+      state: "Finalize",
+    },
+    {
+      id: 4,
+      main: "Tuesday 3:00 pm",
+      sub: "Wednesday 5:00 am KST",
+      isPartnerPick: true,
+      myPick: "sq",
+      state: "Finalize",
+    },
+    {
+      id: 5,
+      main: "Tuesday 3:00 pm",
+      sub: "Wednesday 5:00 am KST",
+      isPartnerPick: true,
+      myPick: "sq",
+      state: "Finalize",
+    },
+  ]);
+
+  const [isChecked, setIsChecked] = useState(false);
+  const [checkedItems, setCheckedItems] = useState(new Set());
+
+  const checkHandler = ({ target }) => {
+    setIsChecked(!isChecked);
+    checkedItemHandler(
+      target.parentNode.parentNode,
+      target.value,
+      target.checked
+    );
+  };
+
+  const checkedItemHandler = (box, id, isChecked) => {
+    if (isChecked) {
+      box.style.backgroundColor = "rgb(248, 243, 233)";
+      box.style.border = "2px solid black";
+      checkedItems.add(id);
+      setTimesData(
+        timesData.map((time) =>
+          time.id == id ? { ...time, state: "Undo" } : time
+        )
+      );
+    } else if (checkedItems.has(id)) {
+      box.style.backgroundColor = "white";
+      box.style.border = "2px solid white";
+      checkedItems.delete(id);
+      setTimesData(
+        timesData.map((time) =>
+          time.id == id ? { ...time, state: "Finalize" } : time
+        )
+      );
+    }
+    setCheckedItems(checkedItems);
+    return checkedItems;
+  };
+
   return (
     <div className={styles.preferredTimes}>
       <h2>Select your preferred times</h2>
@@ -13,15 +91,25 @@ const PreferredTimes = () => {
           <div className={styles.myPick}>Your pick</div>
         </div>
         <div className={styles.tableBody}>
-          {TimesData.map((time, index) => {
+          {timesData.map((time) => {
             return (
-              <div key={index} className={styles.row}>
+              <div key={time.id} className={styles.row}>
                 <div className={styles.overlappingTimes}>
                   <h3>{time.main}</h3>
                   <div>{time.sub}</div>
                 </div>
-                <div className={styles.partnerPick}>{time.partnerPick}</div>
-                <div className={styles.myPick}>{time.myPick}</div>
+                <div className={styles.partnerPick}>
+                  <div className={time.isPartnerPick ? styles.circle : ""}>
+                    {time.isPartnerPick ? "SJ" : ""}
+                  </div>
+                </div>
+                <label className={styles.myPick}>
+                  <input
+                    type="checkbox"
+                    value={time.id}
+                    onChange={(e) => checkHandler(e)}
+                  ></input>
+                </label>
                 <div className={styles.state}>{time.state}</div>
               </div>
             );
--- src/pages/Schedule/Timezone.tsx
+++ src/pages/Schedule/Timezone.tsx
@@ -9,63 +9,156 @@ const Timezone = () => {
   dayjs.extend(utc);
   dayjs.extend(timezone);
 
-  const weekdays = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
+  const [hourUpDown, setHourUpDown] = useState(0);
+  const [minUpDown, setMinUpDown] = useState(0);
+  const [monthUpDown, setMonthUpDown] = useState(0);
+  const [dateUpDown, setDateUpDown] = useState(0);
 
-  const [hourUpDownNY, setHourUpDownNY] = useState(0);
-  const [minUpDownNY, setMinUpDownNY] = useState(0);
-  const [monthUpDownNY, setMonthUpDownNY] = useState(0);
-  const [dateUpDownNY, setDateUpDownNY] = useState(0);
-  const [hourUpDownS, setHourUpDownS] = useState(0);
-  const [minUpDownS, setMinUpDownS] = useState(0);
-  const [monthUpDownS, setMonthUpDownS] = useState(0);
-  const [dateUpDownS, setDateUpDownS] = useState(0);
+  const [hourNY, setHourNY] = useState(
+    dayjs().tz("America/New_York").format("hh")
+  );
+  const [minNY, setMinNY] = useState(
+    dayjs().tz("America/New_York").format("mm")
+  );
+  const [ampmNY, setAmpmNY] = useState(
+    dayjs().tz("America/New_York").format("a")
+  );
+  const [monthNY, setMonthNY] = useState(
+    dayjs().tz("America/New_York").format("MM")
+  );
+  const [dateNY, setDateNY] = useState(
+    dayjs().tz("America/New_York").format("DD")
+  );
+  const [dayNY, setDayNY] = useState(
+    dayjs().tz("America/New_York").format("dddd")
+  );
+  const [hourS, setHourS] = useState(dayjs().format("hh"));
+  const [minS, setMinS] = useState(dayjs().format("mm"));
+  const [ampmS, setAmpmS] = useState(dayjs().format("a"));
+  const [monthS, setMonthS] = useState(dayjs().format("MM"));
+  const [dateS, setDateS] = useState(dayjs().format("DD"));
+  const [dayS, setDayS] = useState(dayjs().format("dddd"));
+
+  var nowNY = dayjs().tz("America/New_York");
+  var nowS = dayjs();
+
+  const setNow = () => {
+    setHourUpDown(0);
+    setMinUpDown(0);
+    setMonthUpDown(0);
+    setDateUpDown(0);
+    setHourNY(dayjs().tz("America/New_York").format("hh"));
+    setMinNY(dayjs().tz("America/New_York").format("mm"));
+    setAmpmNY(dayjs().tz("America/New_York").format("a"));
+    setMonthNY(dayjs().tz("America/New_York").format("MM"));
+    setDateNY(dayjs().tz("America/New_York").format("DD"));
+    setDayNY(dayjs().tz("America/New_York").format("dddd"));
+    setHourS(dayjs().format("hh"));
+    setMinS(dayjs().format("mm"));
+    setAmpmS(dayjs().format("a"));
+    setMonthS(dayjs().format("MM"));
+    setDateS(dayjs().format("DD"));
+    setDayS(dayjs().format("dddd"));
+  };
+
+  const setHour = () => {
+    setHourNY(nowNY.add(hourUpDown, "h").format("hh"));
+    setMinNY(nowNY.add(hourUpDown, "h").format("mm"));
+    setAmpmNY(nowNY.add(hourUpDown, "h").format("a"));
+    setMonthNY(nowNY.add(hourUpDown, "h").format("MM"));
+    setDateNY(nowNY.add(hourUpDown, "h").format("DD"));
+    setDayNY(nowNY.add(hourUpDown, "h").format("dddd"));
+    setHourS(nowS.add(hourUpDown, "h").format("hh"));
+    setMinS(nowS.add(hourUpDown, "h").format("mm"));
+    setAmpmS(nowS.add(hourUpDown, "h").format("a"));
+    setMonthS(nowS.add(hourUpDown, "h").format("MM"));
+    setDateS(nowS.add(hourUpDown, "h").format("DD"));
+    setDayS(nowS.add(hourUpDown, "h").format("dddd"));
+  };
+
+  const setMin = () => {
+    setHourNY(nowNY.add(minUpDown, "m").format("hh"));
+    setMinNY(nowNY.add(minUpDown, "m").format("mm"));
+    setAmpmNY(nowNY.add(minUpDown, "m").format("a"));
+    setMonthNY(nowNY.add(minUpDown, "m").format("MM"));
+    setDateNY(nowNY.add(minUpDown, "m").format("DD"));
+    setDayNY(nowNY.add(minUpDown, "m").format("dddd"));
+    setHourS(nowS.add(minUpDown, "m").format("hh"));
+    setMinS(nowS.add(minUpDown, "m").format("mm"));
+    setAmpmS(nowS.add(minUpDown, "m").format("a"));
+    setMonthS(nowS.add(minUpDown, "m").format("MM"));
+    setDateS(nowS.add(minUpDown, "m").format("DD"));
+    setDayS(nowS.add(minUpDown, "m").format("dddd"));
+  };
+
+  const setMonth = () => {
+    setHourNY(nowNY.add(monthUpDown, "M").format("hh"));
+    setMinNY(nowNY.add(monthUpDown, "M").format("mm"));
+    setAmpmNY(nowNY.add(monthUpDown, "M").format("a"));
+    setMonthNY(nowNY.add(monthUpDown, "M").format("MM"));
+    setDateNY(nowNY.add(monthUpDown, "M").format("DD"));
+    setDayNY(nowNY.add(monthUpDown, "M").format("dddd"));
+    setHourS(nowS.add(monthUpDown, "M").format("hh"));
+    setMinS(nowS.add(monthUpDown, "M").format("mm"));
+    setAmpmS(nowS.add(monthUpDown, "M").format("a"));
+    setMonthS(nowS.add(monthUpDown, "M").format("MM"));
+    setDateS(nowS.add(monthUpDown, "M").format("DD"));
+    setDayS(nowS.add(monthUpDown, "M").format("dddd"));
+  };
+
+  const setDate = () => {
+    setHourNY(nowNY.add(dateUpDown, "d").format("hh"));
+    setMinNY(nowNY.add(dateUpDown, "d").format("mm"));
+    setAmpmNY(nowNY.add(dateUpDown, "d").format("a"));
+    setMonthNY(nowNY.add(dateUpDown, "d").format("MM"));
+    setDateNY(nowNY.add(dateUpDown, "d").format("DD"));
+    setDayNY(nowNY.add(dateUpDown, "d").format("dddd"));
+    setHourS(nowS.add(dateUpDown, "d").format("hh"));
+    setMinS(nowS.add(dateUpDown, "d").format("mm"));
+    setAmpmS(nowS.add(dateUpDown, "d").format("a"));
+    setMonthS(nowS.add(dateUpDown, "d").format("MM"));
+    setDateS(nowS.add(dateUpDown, "d").format("DD"));
+    setDayS(nowS.add(dateUpDown, "d").format("dddd"));
+  };
 
   const setHourUp = () => {
-    setHourUpDownNY(hourUpDownNY + 1);
-    setHourUpDownS(hourUpDownS + 1);
+    setHourUpDown(hourUpDown + 1);
+    setHour();
   };
 
   const setHourDown = () => {
-    setHourUpDownNY(hourUpDownNY - 1);
-    setHourUpDownS(hourUpDownS - 1);
+    setHourUpDown(hourUpDown - 1);
+    setHour();
   };
 
   const setMinUp = () => {
-    setMinUpDownNY(minUpDownNY + 1);
-    setMinUpDownS(minUpDownS + 1);
+    setMinUpDown(minUpDown + 1);
+    setMin();
   };
 
   const setMinDown = () => {
-    setMinUpDownNY(minUpDownNY - 1);
-    setMinUpDownS(minUpDownS - 1);
+    setMinUpDown(minUpDown - 1);
+    setMin();
   };
 
   const setMonthUp = () => {
-    setMonthUpDownNY(monthUpDownNY + 1);
-    setMonthUpDownS(monthUpDownS + 1);
+    setMonthUpDown(monthUpDown + 1);
+    setMonth();
   };
 
   const setMonthDown = () => {
-    setMonthUpDownNY(monthUpDownNY - 1);
-    setMonthUpDownS(monthUpDownS - 1);
+    setMonthUpDown(monthUpDown - 1);
+    setMonth();
   };
 
   const setDateUp = () => {
-    setDateUpDownNY(dateUpDownNY + 1);
-    setDateUpDownS(dateUpDownS + 1);
+    setDateUpDown(dateUpDown + 1);
+    setDate();
   };
 
   const setDateDown = () => {
-    setDateUpDownNY(dateUpDownNY - 1);
-    setDateUpDownS(dateUpDownS - 1);
+    setDateUpDown(dateUpDown - 1);
+    setDate();
   };
 
   return (
@@ -75,35 +168,29 @@ const Timezone = () => {
         <div className={styles.converterItemColored}>
           <div className={styles.city}>
             <div className={styles.cityName}>New York</div>
-            <div className={styles.now}>now</div>
+            <div className={styles.now} onClick={setNow}>
+              now
+            </div>
           </div>
           <div className={styles.time}>
-            <div>
-              {dayjs().tz("America/New_York").format("hh") * 1 + hourUpDownNY}
-            </div>
+            <div>{hourNY}</div>
             <Updown
               width="17px"
               height="15px"
               setUp={setHourUp}
               setDown={setHourDown}
             />
-            <div>
-              {dayjs().tz("America/New_York").format("mm") * 1 + minUpDownNY}
-            </div>
+            <div>{minNY}</div>
             <Updown
               width="17px"
               height="15px"
               setUp={setMinUp}
               setDown={setMinDown}
             />
-            <div className={styles.ampm}>
-              {dayjs().tz("America/New_York").format("a")}
-            </div>
+            <div className={styles.ampm}>{ampmNY}</div>
           </div>
           <div className={styles.date}>
-            <div>
-              {dayjs().tz("America/New_York").format("MM") * 1 + monthUpDownNY}
-            </div>
+            <div>{monthNY}</div>
             <Updown
               width="15px"
               height="10px"
@@ -111,18 +198,14 @@ const Timezone = () => {
               setDown={setMonthDown}
             />
             <div>/</div>
-            <div>
-              {dayjs().tz("America/New_York").format("DD") * 1 + dateUpDownNY}
-            </div>
+            <div>{dateNY}</div>
             <Updown
               width="15px"
               height="10px"
               setUp={setDateUp}
               setDown={setDateDown}
             />
-            <div className={styles.day}>
-              {weekdays[dayjs().tz("America/New_York").day()]}
-            </div>
+            <div className={styles.day}>{dayNY}</div>
           </div>
         </div>
         <div className={styles.converterItem}>
@@ -130,24 +213,24 @@ const Timezone = () => {
             <div className={styles.cityName}>Seoul +14hrs</div>
           </div>
           <div className={styles.time}>
-            <div>{dayjs().format("hh") * 1 + hourUpDownS}</div>
+            <div>{hourS}</div>
             <Updown
               width="17px"
               height="15px"
               setUp={setHourUp}
               setDown={setHourDown}
             />
-            <div>{dayjs().format("mm") * 1 + minUpDownS}</div>
+            <div>{minS}</div>
             <Updown
               width="17px"
               height="15px"
               setUp={setMinUp}
               setDown={setMinDown}
             />
-            <div className={styles.ampm}>{dayjs().format("a")}</div>
+            <div className={styles.ampm}>{ampmS}</div>
           </div>
           <div className={styles.date}>
-            <div>{dayjs().format("MM") * 1 + monthUpDownS}</div>
+            <div>{monthS}</div>
             <Updown
               width="15px"
               height="10px"
@@ -155,14 +238,14 @@ const Timezone = () => {
               setDown={setMonthDown}
             />
             <div>/</div>
-            <div>{dayjs().format("DD") * 1 + dateUpDownS}</div>
+            <div>{dateS}</div>
             <Updown
               width="15px"
               height="10px"
               setUp={setDateUp}
               setDown={setDateDown}
             />
-            <div className={styles.day}>{weekdays[dayjs().day()]}</div>
+            <div className={styles.day}>{dayS}</div>
           </div>
         </div>
       </div>
