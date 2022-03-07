aebc63c9a3d7202e64373078ade72b124a3aa760
haimnam <haimnam10@gmail.com>
2022-03-07 20:12:22 +0900

    Create a ChatInput file and Enable Enter Key to Send

--- src/pages/Schedule/Chat.tsx
+++ src/pages/Schedule/Chat.tsx
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../../scss/ScheduleAndChat.module.scss";
 import ChatInfo from "./ChatInfo.tsx";
 import ChatScreen from "./ChatScreen.tsx";
+import ChatInput from "./ChatInput.tsx";
 
 const Chat = ({ partnerInfoData }) => {
+  const [chat, setChat] = useState({ input: "", chats: [] });
+
+  const onChangeChat = (e) => {
+    setChat({ input: e.target.value, chats: chat.chats });
+  };
+  const onClickChat = (e) => {
+    setChat({ input: "", chats: chat.chats.concat(chat.input) });
+  };
+  const onKeyPressChat = (e) => {
+    if (e.key === "Enter") {
+      onClickChat(e);
+    }
+  };
+
   return (
     <div className={styles.chatBody}>
       <ChatInfo partnerInfoData={partnerInfoData} />
-      <ChatScreen />
+      <div className={styles.chatScreen}>
+        <ChatScreen chat={chat} />
+        <ChatInput
+          chat={chat}
+          onChangeChat={onChangeChat}
+          onClickChat={onClickChat}
+          onKeyPressChat={onKeyPressChat}
+        />
+      </div>
     </div>
   );
 };
